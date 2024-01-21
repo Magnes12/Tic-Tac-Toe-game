5b97b89cf40ca586e45385c1030d09b7f044b18e
Damian <m4gn3s@gmail.com>
2024-01-21 21:45:30 +0100

    kk

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 clear = lambda: os.system('cls')
 
+
 def display_board(board):
 
     print(f" {board[0]} | {board[1]} | {board[2]} ")
@@ -17,18 +18,18 @@ board = [' 0 ', ' 1 ', ' 2 ', ' 3 ', ' 4 ', ' 5 ', ' 6 ', ' 7 ', ' 8 ']
 def player_x():
 
     player_x = int(input("Which field? (X) "))
-    if board[player_x] !=" O ":
+    if board[player_x] != " O ":
         board[player_x] = " X "
     else:
         print("This field seems taken")
         player_x_2 = int(input("Choose another field? (X) "))
         board[player_x_2] = " X "
-    
+
 
 def player_o():
 
     player_o = int(input("Which field? (O) "))
-    if board[player_o] !=" X ":
+    if board[player_o] != " X ":
         board[player_o] = " O "
     else:
         print("This field seems taken")
@@ -37,20 +38,20 @@ def player_o():
 
     clear()
 
+
 def check_if_win(board):
     win_comb = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],  
-        [0, 4, 8], [2, 4, 6]             
-    ]
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6],
+        ]
     for com in win_comb:
         if all(board[pos] == " X " for pos in com):
             return True
         elif all(board[pos] == " O " for pos in com):
             return True
-    
     return False
-            
+
 
 game_is_on = True
 
@@ -67,4 +68,3 @@ while game_is_on:
         display_board(board)
         print("Player X wins!")
         break
-
